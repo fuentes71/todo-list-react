c8e38ad5cb428ede0f59a153cc4ed86de715b279
Matheus <matheus.fu.p@hotmail.com>
2023-02-20 21:21:55 -0300

    add react router dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<p>pag inicial</p>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
